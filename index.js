82c13eaf06e37e911114b6bddf6a2517860337ec
Petosz02 <78482842+Petosz02@users.noreply.github.com>
2021-05-09 12:53:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ var randomNumber1 = 5, randomNumber2 = 5;
 var dice1 = document.getElementById("dice1");
 var dice2 = document.getElementById("dice2");
 
-dice1.src = "images/dice" + (randomNumber1 + 1) + ".png";
-dice2.src = "images/dice" + (randomNumber2 + 1) + ".png";
+
 
 var winnerString;
-//var dicee = document.querrySelectorAll("img");
+var dicee = document.querySelectorAll("img");
 
 document.getElementById("roll").addEventListener("click", roll);
 
+dicee[0].src = "images/dice" + (randomNumber1 + 1) + ".png";
+dicee[1].src = "images/dice" + (randomNumber2 + 1) + ".png";
 
 console.log(randomNumber1 + " " + randomNumber2);
 console.log(dice1);
@@ -20,8 +21,8 @@ function roll(){
   randomNumber2 = Math.floor( Math.random() * 6);
   randomNumber1 = Math.floor( Math.random() * 6);
 
-  dice1.src = "images/dice" + (randomNumber1 + 1) + ".png";
-  dice2.src = "images/dice" + (randomNumber2 + 1) + ".png";
+  dicee[0].src = "images/dice" + (randomNumber1 + 1) + ".png";
+  dicee[1].src = "images/dice" + (randomNumber2 + 1) + ".png";
 
   if (randomNumber1 > randomNumber2) {
     winnerString = "Player 1 won!";
